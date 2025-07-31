e55589e0803dc93186864e201a1f0fb28f5c274a
keshav-repo <keshavrohilla987@gmail.com>
2025-07-31 23:59:29 +0530

    added login file and validate the input with usermodel

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {body , validationResult} = require('express-validator')
 const userModel = require('../models/user.model')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 router.get('/register' , (req,res)=>{
     res.render('register')
 })
@@ -29,5 +30,47 @@ router.post('/register',
     })
     res.json(newUser)
 })
+
+router.get('/login' , (req,res)=>{
+    res.render('login')
+})
+router.post('/login' , 
+    body('username').trim().isEmail().isLength({min:0}),
+    body('password').trim().isLength({min : 0}),
+    async (req,res)=>{
+        const errors =  validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                error: errors.array() , 
+                message : 'Invalid data'
+            })
+        }
+        const {username , password} = req.body;
+        const user = await userModel.findOne({
+            username: username,
+            
+        })
+        if(!user) {
+            return res.status(400).json({
+                message : 'username or password is incorrect'
+            })
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch) {
+            return res.status(400).json({
+                message: 'username or password is incorrect'
+            })
+        }
+
+        const token = jwt.sign({
+            userId:user._id,
+            email: user.email,
+            username : user.username
+        },
+          process.env.JWT_SECRET
+        )
+        res.json({token})
+    }
+)
 
 module.exports = router
